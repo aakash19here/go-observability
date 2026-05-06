a89e29da8a6892db015b0e4ddf9d561c1cd1f6cf
Aakash <aakash19here@gmail.com>
2026-05-06 12:27:22 +0530

    chore: add stack traces to the logs

--- main.go
+++ main.go
@@ -30,9 +30,24 @@ func main() {
 type closeFunc func() error
 
 func initializeLogger(logFile string) (*slog.Logger, closeFunc, error) {
+
+	replaceAttr := func(groups []string, a slog.Attr) slog.Attr {
+		if a.Key == "error" {
+			err, ok := a.Value.Any().(error)
+
+			if !ok {
+				return a
+			}
+
+			return slog.String("error", fmt.Sprintf("%+v", err))
+		}
+		return a
+	}
+
 	handlers := []slog.Handler{
 		slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{
-			Level: slog.LevelDebug,
+			Level:       slog.LevelDebug,
+			ReplaceAttr: replaceAttr,
 		}),
 	}
 	closers := []closeFunc{}
@@ -53,7 +68,8 @@ func initializeLogger(logFile string) (*slog.Logger, closeFunc, error) {
 			return nil
 		}
 		handlers = append(handlers, slog.NewJSONHandler(bufferedFile, &slog.HandlerOptions{
-			Level: slog.LevelInfo,
+			Level:       slog.LevelInfo,
+			ReplaceAttr: replaceAttr,
 		}))
 		closers = append(closers, close)
 	}
